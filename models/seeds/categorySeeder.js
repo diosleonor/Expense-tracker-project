dd597575e464965468969007b412ce0fc1e70d3f
diosleonor <tccoolovl@gmail.com>
2022-01-13 11:36:43 +0800

    add categoriesSeeder

---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,12 @@
+if(process.env.NODE_ENV !== 'production'){
+	require('dotenv').config()
+}
+const Category = require('../Category')
+const db = require('../../config/mongoose')
+const SEED_CATEGORIES = require("../seedData.json").SEED_CATEGORIES
+
+db.once('open', async () => {
+	console.log('Mongodb connected.')
+	await Category.create(SEED_CATEGORIES)
+	console.log('Categories created.')
+})
