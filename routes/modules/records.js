530975eb2de05c25b1a5654add5777df69bb97ec
diosleonor <tccoolovl@gmail.com>
2022-01-14 14:02:46 +0800

    add edit record page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -6,4 +6,16 @@ router.get('/new', (req, res) => {
 	return res.render('new')
 })
 
+router.get('/:id/edit', (req, res) => {
+	// const userId = req.user._id
+	const _id = req.params.id
+	return Record.findOne({_id})
+		.lean()
+		.then(record => res.render('edit', { record }))
+		.catch(error => console.log(error))
+})
+
+
+
+
 module.exports = router
