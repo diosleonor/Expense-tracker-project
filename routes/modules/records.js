d588cfd4d1f57a4f99b692ef265158a7b737a677
diosleonor <tccoolovl@gmail.com>
2022-01-14 14:10:23 +0800

    add delete record function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 
+// Create
 router.get('/new', (req, res) => {
 	return res.render('new')
 })
 
+
+
+// Update
 router.get('/:id/edit', (req, res) => {
 	// const userId = req.user._id
 	const _id = req.params.id
@@ -16,6 +20,16 @@ router.get('/:id/edit', (req, res) => {
 })
 
 
+
+// Delete
+router.delete('/:id', (req, res) => {
+	// const userId = req.user._id
+	const _id = req.params.id
+	return Record.findOne({_id})
+		.then(record => record.remove())
+		.then(() => res.redirect('/'))
+		.catch(error => console.log(error))
+})
 
 
 module.exports = router
