1ad6fe1988afea885dc79018b7af308642089fc1
diosleonor <tccoolovl@gmail.com>
2022-01-14 14:47:14 +0800

    add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,9 +6,43 @@ const bcrypt = require('bcryptjs')
 router.get('/login', (req, res) => {
 	return res.render('login')
 })
+// router.post('/login', passport.authenticate('local', {
+// 	successRedirect:'/',
+// 	failureRedirect:'/login'
+// }))
 
 router.get('/register', (req, res) => {
 	return res.render('register')
 })
-
+router.post('/register', (req, res) => {
+ const {name, email, password, confirmPassword} = req.body
+ const errors = []
+ if(!name||!email||!password||!confirmPassword){
+ 	errors.push({message:'所有欄位都是必填。'})
+ }
+ if(password!==confirmPassword){
+ 	errors.push({message:'密碼與確認密碼不相符！'})
+ }
+ if(errors.length){
+ 	return res.render('register', {name, email, password, confirmPassword, errors})
+ }
+ User.findOne({ email }).then(user => {
+ 	if(user){
+	 	errors.push({message:'這個 Email 已經註冊過了。'})
+ 		return res.render('register',{name, email, password, confirmPassword, errors})
+ 	}
+ 	{
+ 	return bcrypt
+ 		.genSalt(10) // produce salt and set the complexity as 10
+ 		.then(salt => bcrypt.hash(password, salt)) // hash password and salt
+ 		.then(hash => User.create({
+			name, 
+ 			email, 
+ 			password: hash // asign hash as password
+		}))
+ 		.then(() => res.redirect('/'))
+ 		.catch(err => console.log(err))
+ 	}
+ })
+})
 module.exports = router
